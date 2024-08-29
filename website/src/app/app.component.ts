3b9abf1dd69608365ff1ffdb955ff40c1d8c8be3
Fares Raboudi <raboudi@ngsign-dev6.ngsign.local>
2024-08-29 14:14:44 +0100

    Added translation & internalization infrastructure (Translation feature still Not ready)

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { en_US, fr_FR, NzI18nService } from 'ng-zorro-antd/i18n';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'website';
+
+  constructor(
+    private translate: TranslateService,
+    private nzI18nService: NzI18nService
+  ) {
+    const browserLang = navigator.language;
+    if (browserLang && browserLang.toUpperCase().includes('FR')) {
+      translate.setDefaultLang('FR');
+      this.nzI18nService.setLocale(fr_FR);
+    } else {
+      translate.setDefaultLang('EN');
+      this.nzI18nService.setLocale(en_US);
+    }
+  }
 }
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomNavbarModule } from './custom-navbar/custom-navbar.module';
 import { SectionsModule } from './sections/sections.module';
 import { FooterModule } from './footer/footer.module';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+// registerLocaleData(en);
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/');
+}
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -15,8 +29,18 @@ import { FooterModule } from './footer/footer.module';
     CustomNavbarModule,
     SectionsModule,
     FooterModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
