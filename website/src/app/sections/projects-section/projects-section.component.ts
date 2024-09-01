b8e82a228157832f420eaf851c1cd1dc60b5afb5
Fares <faresraboudi308@gmail.com>
2024-09-01 01:21:51 +0100

    Changes in assets and deployment folders.

--- website/src/app/sections/projects-section/projects-section.component.ts
+++ website/src/app/sections/projects-section/projects-section.component.ts
@@ -9,17 +9,17 @@ export class ProjectsSectionComponent {
   projects: ProjectInfo[] = [
     {
       projectTitle: 'Book Social Network',
-      coverImageUrl: './assets/BSN.jpg',
+      coverImageUrl: './assets/projects/BSN.jpg',
       githubLink: 'https://github.com/Fares-Rbd/book-social-network',
     },
     {
       projectTitle: 'Hotel Management Web App',
-      coverImageUrl: './assets/HM.png',
+      coverImageUrl: './assets/projects/HM.png',
       githubLink: 'https://github.com/Fares-Rbd/JEE_Projects/tree/master',
     },
     {
       projectTitle: 'Library Web Application',
-      coverImageUrl: './assets/BM.png',
+      coverImageUrl: './assets/projects/BM.png',
       githubLink: 'https://github.com/Fares-Rbd/LibraryWebApp/',
     },
   ];
